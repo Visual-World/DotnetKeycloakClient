def81ab45d0f47e7e7632a6f3d8b9eb758c6eb8b
Jan Hafner <jan.hafner@visual-world.de>
2023-06-22 10:53:10 +0200

    Initial commit

---
+++ VisualWorld.Keycloak/Registrations.cs
@@ -0,0 +1,42 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using VisualWorld.Keycloak;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class Registrations
+{
+    public static IServiceCollection AddKeycloakClient(this IServiceCollection services, Action<KeycloakOptions>? configure = null)
+    {
+        var keycloakOptionsBuilder = services.AddOptions<KeycloakOptions>();
+        if (configure is not null)
+        {
+            keycloakOptionsBuilder.Configure(configure);
+        }
+
+        services.AddAccessTokenManagement((sp, options) =>
+        {
+            var keycloakOptions = sp.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+            
+            options.Client.Clients.Add("KeycloakClient", new ClientCredentialsTokenRequest
+            {
+                ClientId = keycloakOptions.ClientId,
+                ClientSecret = keycloakOptions.ClientSecret,
+                Address = keycloakOptions.TokenUrl,
+            });
+        });
+        services.AddKeycloakApiClient().AddClientAccessTokenHandler();
+
+        return services;
+    }
+
+    private static IHttpClientBuilder AddKeycloakApiClient(this IServiceCollection services)
+        => services
+            .AddScoped<IKeycloakClient, KeycloakClient>()
+            .AddHttpClient<IKeycloakClient, KeycloakClient>((sp, httpClientBuilder) =>
+            {
+                var keycloakOptions = sp.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+                httpClientBuilder.BaseAddress = new Uri(keycloakOptions.ApiUrl);
+            });
+}
